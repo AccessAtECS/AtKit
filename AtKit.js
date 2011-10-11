5e887b6d91351e01256b59bf60b2a2a4406b2b70
Sebastian Skuse <seb@skuse.net>
2011-10-11 15:09:42 +0100

    Updated build

--- AtKit.js
+++ AtKit.js
@@ -17,7 +17,7 @@
 		// Internal properties
 		AtKit.internal = AtKit.prototype = {
 			__version: 1.0, // Version.
-			__build: 130, // Build.
+			__build: 135, // Build.
 			__baseURL: "http://c.atbar.org/", // Load AtKit assets from here.
 			__APIURL: "http://a.atbar.org/", // API endpoint
 			__libURL: "http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js", // URL to jQuery. CDN preferred unless this is a local install.
